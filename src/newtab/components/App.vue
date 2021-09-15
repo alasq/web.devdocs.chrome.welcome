c2b4fd420cea2159531711e074991d67743082c0
陈诗樵 <1073935487@qq.com>
2021-09-16 00:20:08 +0800

    骨架完成

---
+++ src/newtab/components/App.vue
@@ -0,0 +1,125 @@
+<template>
+  <div>
+    <span
+      >Only For Chinese FrontEnd Developer! If you like this,you can fork this
+      <a href="https://github.com/alasq/web.devdocs.chrome.welcome">repo</a> and
+      edit it.</span
+    >
+  </div>
+  <div class="search-box">
+    <input v-model="searchText" type="search" placeholder="请输入关键字搜索" />
+  </div>
+  <div class="flex">
+    <div v-for="(group, index) of sortedData" :key="index">
+      <h3>{{ group.title }}</h3>
+      <ul>
+        <li
+          v-for="(item, key) of group.list"
+          :key="key"
+          :style="{
+            'background-image':
+              'url(' +
+              (item.icon ||
+                'https://www.google.cn/chrome/static/images/chrome-logo.svg') +
+              ')',
+          }"
+        >
+          <a
+            :href="item.url"
+            :class="{ hight: hight(item.title) }"
+            @click.prevent="itemClick(item)"
+          >
+            {{ item.title }}</a
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+import localforage from 'localforage'
+import { toRaw } from '@vue/reactivity'
+
+const data = []
+const files = require.context('../../assets/', false, /\.ya?ml$/)
+files.keys().forEach((key) => {
+  data.push(files(key))
+})
+
+export default {
+  data() {
+    return {
+      data,
+      count: null,
+      searchText: '',
+    }
+  },
+  computed: {
+    sortedData() {
+      if (this.count) {
+        this.data.forEach(({ list }) => {
+          list.sort(
+            (a, b) => (this.count[b.url] || 0) - (this.count[a.url] || 0)
+          )
+        })
+      }
+
+      return this.data
+    },
+  },
+  async created() {
+    this.count = await localforage.getItem('count')
+  },
+  methods: {
+    async itemClick(item) {
+      const count = this.count || {}
+      count[item.url] = (count[item.url] || 0) + 1
+      await localforage.setItem('count', toRaw(count))
+      window.location.href = item.url
+    },
+    hight(title) {
+      if (!this.searchText) {
+        return false
+      }
+      return new RegExp(this.searchText, 'i').test(title)
+    },
+  },
+}
+</script>
+<style>
+li a {
+  font-size: 16px;
+}
+.flex {
+  display: flex;
+}
+.flex > div {
+  padding: 10px;
+}
+.hight {
+  background: yellow;
+}
+.search-box {
+  text-align: center;
+  margin-top: 50px;
+}
+.search-box input {
+  width: 50%;
+  height: 40px;
+  font-size: 18px;
+  border-radius: 20px;
+  padding-left: 20px;
+  border: 2px solid 2px solid #39efb4;
+}
+
+ul {
+  padding-left: 0;
+}
+ul li {
+  list-style-type: none;
+  background: url('https://www.google.cn/chrome/static/images/chrome-logo.svg')
+    no-repeat 0rem 0.3rem;
+  background-size: 1rem 1rem;
+  text-indent: 2em;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const path = require('path')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+
+new webpack.DefinePlugin({
+  __VUE_PROD_DEVTOOLS__: false,
+})
+
+const mode = process.env.NODE_ENV || 'development'
+const prod = mode === 'production'
+
+const config = {
+  mode: prod ? 'production' : 'development',
+  devtool: prod ? 'source-map' : 'eval-cheap-module-source-map',
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    options: ['./option/option.js'],
+    newtab: ['./newtab/newtab.js'],
+    background: ['./background/background.js'],
+    content: ['./content/content.js'],
+  },
+  resolve: {
+    alias: {
+      vue: '@vue/runtime-dom',
+    },
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    chunkFilename: '[name].[id].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          extractCSS: prod,
+        },
+      },
+      {
+        test: /\.png$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 8192 },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.ya?ml$/,
+        use:['json-loader','yaml-loader']
+      }
+    ],
+  },
+  mode,
+  plugins: [
+    new VueLoaderPlugin(),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Options',
+      template: './index.html',
+      filename: 'option.html',
+      chunks: ['options'],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'New Tab',
+      template: './index.html',
+      filename: 'newtab.html',
+      chunks: ['newtab'],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'assets', to: 'assets' },
+        { from: 'manifest.json', to: 'manifest.json' },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  stats: {
+    assetsSort: '!size',
+    children: false,
+    usedExports: false,
+    modules: false,
+    entrypoints: false,
+    // Hide source maps from output
+    excludeAssets: [/\.*\.map/],
+  },
+}
+
+module.exports = config
